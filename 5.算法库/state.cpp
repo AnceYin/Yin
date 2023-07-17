21b042147c6e528454b06358a1a037c3bf900604
Ance <117335537+AnceYin@users.noreply.github.com>
2023-07-18 00:23:20 +0800

    Create state.cpp

---
+++ 5.算法库/state.cpp
@@ -0,0 +1,24 @@
+// 实现多态的两种方法
+#include <variant>
+#include <functional>
+
+void test01() {
+	struct A { void operator()() { printf("逻辑A\n"); } };
+	struct B { void operator()() { printf("逻辑B\n"); } };
+	std::variant<A, B> state{A{}};
+	std::visit([](auto&& s) {s(); }, state);
+	state = B{};
+	std::visit([](auto&& s) {s(); }, state);
+}
+
+void test02() {
+	auto a = []() {printf("逻辑A\n"); };
+	auto b = []() {printf("逻辑B\n"); };
+	auto c = [](std::function<void()> f) {f(); };
+	c(a);
+	c(b);
+}
+
+int main() {
+	test01();
+}
