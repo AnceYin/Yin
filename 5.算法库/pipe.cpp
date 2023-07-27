89295bc5162241e8d9e275fc2dc8d93d446fcd4d
Ance <117335537+AnceYin@users.noreply.github.com>
2023-07-28 00:36:52 +0800

    Update pipe.cpp

--- 5.算法库/pipe.cpp
+++ 5.算法库/pipe.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <tuple>
 
 template<typename D, typename F>
-D operator|(D d, F f) {
-    return d = f(d), d;
+auto operator|(D d, F f) {
+    return f(d);
 }
+
 
 int main() {
     const int i = 3;
-    std::cout << (i | [](auto i) { return i - 1; } | [](auto i) { return i + 1; });
+    std::cout << (i | [](auto i) { return std::make_tuple(10, "string"); } | [](std::tuple<int, std::string> data) { return std::get<1>(data); });
     return 0;
 }
